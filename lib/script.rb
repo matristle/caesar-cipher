d3c84a9db15262b058428e599145416a665ccf96
matristle <97355883+matristle@users.noreply.github.com>
2022-05-31 14:32:23 +0300

    Refactor count assignment for cycle stage

--- lib/script.rb
+++ lib/script.rb
@@ -9,25 +9,21 @@ def check_and_transform(ordinal_number, shift_factor)
 
   shifted_ordinal_number = ordinal_number + shift_factor
 
-  if shifted_ordinal_number.in_either_bounds?
-    return shifted_ordinal_number
+  return shifted_ordinal_number if shifted_ordinal_number.in_either_bounds?
+
+  lower_limit, upper_limit = 65, 90  if ordinal_number.between?(65, 90)
+  lower_limit, upper_limit = 97, 122 if ordinal_number.between?(97, 122)
+
+  if shift_factor.positive?
+    count = (upper_limit - ordinal_number) + 1
+    return lower_limit - (count - shift_factor)
+  elsif shift_factor.negative?
+    count = (ordinal_number - lower_limit) + 1
+
+    return upper_limit + (count + shift_factor)
   else
-    lower_limit, upper_limit = 65, 90  if ordinal_number.between?(65, 90)
-    lower_limit, upper_limit = 97, 122 if ordinal_number.between?(97, 122)
-    count = 0
 
-    if shift_factor.positive?
-      ordinal_number.upto(upper_limit) { count += 1 }
-
-      return lower_limit - (count - shift_factor)
-    elsif shift_factor.negative?
-      ordinal_number.downto(lower_limit) { count += 1 }
-
-      return upper_limit + (count + shift_factor)
-    else
-
-      return ordinal_number
-    end
+    return ordinal_number
   end
 end
 
@@ -39,16 +35,16 @@ end
 
 #-- Tests by Output -----------------------------------------------------------------------------------------
 
-puts caesar_cipher('ABCDEFGHIJKLMNOP', 1)
-puts caesar_cipher('ABCDEFGHIJKLMNOP'.downcase, 1)
+# puts caesar_cipher('ABCDEFGHIJKLMNOP', 1)
+# puts caesar_cipher('ABCDEFGHIJKLMNOP'.downcase, 1)
 
-puts caesar_cipher('ABCDEFGHIJKLMNOP', -1)
-puts caesar_cipher('ABCDEFGHIJKLMNOP'.downcase, -1)
+# puts caesar_cipher('ABCDEFGHIJKLMNOP', -1)
+# puts caesar_cipher('ABCDEFGHIJKLMNOP'.downcase, -1)
 
-puts caesar_cipher('WHERE ARE MY SLIPPERS?', -1)
-puts caesar_cipher('Why are you mocking me?!', -2)
+# puts caesar_cipher('WHERE ARE MY SLIPPERS?', -1)
+# puts caesar_cipher('Why are you mocking me?!', -2)
 
-puts caesar_cipher('this is so low-key', 1)
+# puts caesar_cipher('this is so low-key', 1)
 puts caesar_cipher('What a string!', 5)
 puts caesar_cipher('My name is... um, actually: I forgot. What was it?', 10)
 
